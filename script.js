f2084dc026a6acaffb3f10296078e5afec4c99ed
rexram987-create <rexram987@gmail.com>
2026-08-05 19:41:40 +0300

    Load verified 1949 Billboard jukebox chart

--- script.js
+++ script.js
@@ -51,6 +51,7 @@ async function loadData() {
       'data/charts-1946-radio-jukebox.json',
       'data/charts-1947-radio-jukebox.json',
       'data/charts-1948-radio.json',
+      'data/charts-1949-jukebox.json',
       'data/combined-1946-1949.json',
       'data/charts-1950-1957.json',
       'data/charts-1959-1963.json',
